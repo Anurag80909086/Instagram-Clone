6834933d7d0c518318115a27a7353f607d7eaabb
Anurag80909086 <anurag.singh26@amity.edu>
2024-09-21 23:18:16 +0530

    finished signin

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -1,4 +1,5 @@
 import "./SignIn.css";
+import { IoLogoFacebook } from "react-icons/io";
 import InstaLogoBlack from "../../components/Insta_Logo_Black/InstaLogoBlack.jsx";
 const SignIn = () => {
   return (
@@ -6,12 +7,35 @@ const SignIn = () => {
       <div className="loginWrapper flex">
         <InstaLogoBlack />
         <form action="submit" className="flex">
-          <input type="email" placeholder="Phone number, username, or email" />
-          <input type="password" placeholder="Password" />
+          <input
+            type="email"
+            required
+            placeholder="Phone number, username, or email"
+          />
+          <input type="password" required placeholder="Password" />
           <button type="submit">Log in</button>
         </form>
+
+        <div className="devider flex">
+          <div className="devidebar"></div>
+          <span>OR</span>
+          <div className="devidebar"></div>
+        </div>
+
+        <div className="optionLogin flex">
+          <div className="facebookOption flex">
+            <IoLogoFacebook className="facebookIcon" /> Login with Facebook
+          </div>
+          <a href="#" className="textCenter">
+            Forget password?
+          </a>
+        </div>
       </div>
-      <div className="lowerPart"></div>
+      <div className="lowerPart flex">
+        <div className="text">
+          Do you have an account? <span id="signup">Sign up</span>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,37 +1,5 @@
-import { FaFacebookSquare } from "react-icons/fa";
-import InstaLogoBlack from "../../components/Insta_Logo_Black/InstaLogoBlack";
-import "./SignUp.css";
-function SignUp() {
-  return (
-    <div>
-      <div>
-        <InstaLogoBlack />
-      </div>
-      <div>
-        <form>
-          <div>
-            <div>
-              <span>Sign up to see photos and videos from your friends.</span>
-            </div>
-            <div>
-              <button>
-                <span>
-                  <FaFacebookSquare />
-                </span>
-                Login With Facebook
-              </button>
-            </div>
-            <div className="flex">
-              <hr />
-              <div>O</div>
-              <hr />
-            </div>
-          </div>
-        </form>
-      </div>
-      <div></div>
-    </div>
-  );
-}
+const SignUp = () => {
+  return <div>SignUp</div>;
+};
 
 export default SignUp;
