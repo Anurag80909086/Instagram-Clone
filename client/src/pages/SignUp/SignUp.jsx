be6ce3f30b5c4968ed77fa636c7b2bee4e514a03
AvhaleSwapnil <swapnilavhale460@gmail.com>
2024-09-21 13:26:31 +0530

    change 4

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,21 @@
 import "./SignUp.css";
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <div>logo</div>
+        </div>
+      </div>
+      <div>
+        <form>
+          <div>
+            <span>Sign up to see photos and videos from your friends.</span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
