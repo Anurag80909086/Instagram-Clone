b37a9029be63f5786f621a70924290340e3726f7
Anurag80909086 <anurag.singh26@amity.edu>
2024-09-21 15:06:10 +0530

    added login

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -3,12 +3,15 @@ import InstaLogoBlack from "../../components/Insta_Logo_Black/InstaLogoBlack.jsx
 const SignIn = () => {
   return (
     <div className="login flex">
-      <div className="loginWrapper">
+      <div className="loginWrapper flex">
         <InstaLogoBlack />
+        <form action="submit" className="flex">
+          <input type="email" placeholder="Phone number, username, or email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Log in</button>
+        </form>
       </div>
-      <div className="lowerPart">
-
-      </div>
+      <div className="lowerPart"></div>
     </div>
   );
 };
