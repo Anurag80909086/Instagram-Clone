e4ac2544b2384a50643a42978c146917d7e9fe28
AvhaleSwapnil <swapnilavhale460@gmail.com>
2024-09-23 16:36:24 +0530

    change

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,35 +1,36 @@
-import { FaFacebookSquare } from "react-icons/fa";
+import { AiFillFacebook } from "react-icons/ai";
 import InstaLogoBlack from "../../components/Insta_Logo_Black/InstaLogoBlack";
 import "./SignUp.css";
 function SignUp() {
   return (
-    <div>
-      <div>
+    <div className="signup flex">
+      <div className="signupWrapper flex">
         <InstaLogoBlack />
-      </div>
-      <div>
-        <form>
-          <div>
-            <div>
-              <span>Sign up to see photos and videos from your friends.</span>
-            </div>
-            <div>
-              <button>
-                <span>
-                  <FaFacebookSquare />
-                </span>
-                Login With Facebook
-              </button>
-            </div>
-            <div className="flex">
-              <hr />
-              <div>O</div>
-              <hr />
-            </div>
+        <div className="flex">
+          <span>
+            Sign up to see photos and videos <br /> from your friends.
+          </span>
+          <button className="flex">
+            <AiFillFacebook className="facebookIcon" />
+            Log in with Facebook
+          </button>
+        </div>
+        <form action="submit" className="flex">
+          <input type="email" placeholder="Mobile Number or Email" />
+          <input type="text" placeholder="Full Name" />
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <div className="flex">
+            <span>
+              People who use our service may have uploaded your contact
+              information to Instagram.
+              <a>Learn More</a>
+            </span>
           </div>
+          <button type="submit">Log in</button>
         </form>
       </div>
-      <div></div>
+      <div className="lowerPart"></div>
     </div>
   );
 }
